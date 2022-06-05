43c869f25a6e5f580b5315a7aebc615f537167df
linchuaccount <younchu123@gmail.com>
2022-06-05 15:04:03 +0800

    feat: set template engine

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 const express = require('express')
 const app = express() //用app來代表呼叫express()，呼叫express()會啟動Express應用程式伺服器
 const port = 3000
+//載入樣板引擎
+const { engine } = require('express-handlebars')
+
+app.engine('.hbs', engine({ extname: '.hbs',defaultLayour: 'main' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 
 app.get('/',(req,res)=>{
-    res.send('Hello everyone')
+    res.render('index')
 })
 
 //設定伺服器監聽port3000，應用程式會處理任何從port3000進來的瀏覽器request
