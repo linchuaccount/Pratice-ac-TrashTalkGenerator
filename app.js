f0843e1c6e643518f71a45bb912c6ecd58d41789
linchuaccount <younchu123@gmail.com>
2022-06-05 22:56:44 +0800

    feat: add handlebars-helper for keep input checked

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 const express = require('express')
 const app = express() //用app來代表呼叫express()，呼叫express()會啟動Express應用程式伺服器
 const port = 3000
+const handlebars = require('handlebars')
 //載入樣板引擎
 const { engine } = require('express-handlebars')
 //載入js功能
-const generatorTrashtalk = require('./generator_trashtalk')
+const generatorTrashtalk = require('./public/javascripts/generator_trashtalk')
+
 
 app.engine('.hbs', engine({ extname: '.hbs',defaultLayour: 'main' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
+
 
 //載入靜態檔案
 app.use(express.static('public'))
@@ -20,6 +23,12 @@ app.get('/',(req,res)=>{
     res.render('index', { trashtalk, option })
 })
 
+//Handlebars-helper
+handlebars.registerHelper('check', function( userSelect, occupation, options) {
+    if ( userSelect === occupation ) {
+        return options.fn(this)
+    }
+})
 
 //設定伺服器監聽port3000，應用程式會處理任何從port3000進來的瀏覽器request
 app.listen(port, ()=>{
